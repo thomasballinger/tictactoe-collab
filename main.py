4cb77f543cc93c634f76446ce9f7a60f8f010136
Thomas Ballinger <thomasballinger@gmail.com>
2015-01-05 15:20:02 -0500

    added bounds checking to input

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ board = [[None] * 3 for _ in range(3)]
 print board
 while True:
     x, y = [int(x) for x in raw_input().split()]
-    board[y][x] = 'x'
-    print board
+    if 0 <= x <= 2 and 0 <= y <= 2:
+        board[y][x] = 'x'
+        print board
