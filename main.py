2b4f7543f8ca0a0551537bb476fda4c3d3d271b7
Thomas Ballinger <thomasballinger@gmail.com>
2015-01-05 15:18:56 -0500

    using tuple unpacking to clarify spot -> x, y

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ board = [[None] * 3 for _ in range(3)]
 
 print board
 while True:
-    spot = [int(x) for x in raw_input().split()]
-    board[spot[1]][spot[0]] = 'x'
+    x, y = [int(x) for x in raw_input().split()]
+    board[y][x] = 'x'
     print board
